1a2dfd551cf75261948c73901eb51ba8e35d4def
Kakapo <kacper20015@o2.pl>
2025-12-29 21:27:32 +0100

    small

--- Taxi/dqnParams.py
+++ Taxi/dqnParams.py
@@ -6,27 +6,25 @@ from collections import deque
 import qLearningParams
 
 state_size = 2+2*(len(GameParams.POSSIBLE_SPAWNS)+1) # [px, py, passenger_id, destination_id]
+print("STATE SIZE", state_size)
 action_size = 6 # 0 up, 1 right, 2 down, 3 left , 4 pick up, 5 drop
 
 model = nn.Sequential(
-            nn.Linear(state_size, 64),
-            nn.ReLU(),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Linear(64, action_size)
-        )
+    nn.Linear(state_size, 64),
+    nn.ReLU(),
+    nn.Linear(64, 64),
+    nn.ReLU(),
+    nn.Linear(64, action_size)
+)
 
 model_target = nn.Sequential(
-            nn.Linear(state_size, 64),
-            nn.ReLU(),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Linear(64, action_size)
-        )
+    nn.Linear(state_size, 64),
+    nn.ReLU(),
+    nn.Linear(64, 64),
+    nn.ReLU(),
+    nn.Linear(64, action_size)
+)
+
 model_target.load_state_dict(model.state_dict())
 
 optimizer = optim.Adam(model.parameters(), lr=qLearningParams.learningRate)
